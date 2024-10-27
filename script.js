ecba58f676b569af8a095bad829740babcbea928
Myname <myemail@gmail.com>
2024-10-27 22:56:01 +0100

    Implementazione delel immagini fino a prince of persia the sand of time
    Nel file home.html ho assegnatogli id alle varie carte mentre nel file javascript, ho assegnato le immagini alla classe di oggetti, e implementato la funzione, che  in base all'Id mi restituisce l'immagine grande.

--- script.js
+++ script.js
@@ -36,21 +36,21 @@ const relatedImages = {
     ],
     //qua ci andranno le immagini per il blocco 2
     related2: [
-        'related2-1.jpg',
-        'related2-2.jpg',
-        'related2-3.jpg'
+        'https://i.ytimg.com/vi/Ki3X1tfGFNM/maxresdefault.jpg',
+        'https://storage.ensigame.com/games/eb87b59c0dd7f5fd58b9a6e090c1148b.jpg',
+        'https://dosgames.com/screens/prince.gif'
     ],
     //qua ci andranno le immagini per il blocco 3
     related3: [
-        'related3-1.jpg',
-        'related3-2.jpg',
-        'related3-3.jpg'
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-GfxrqdvDpVm45fZUWNpu4inQgvivmWokmQ&srelated3-1.jpg',
+        'https://lutris.net/media/games/screenshots/s1.png',
+        'https://preview.redd.it/have-anyone-played-this-game-prince-od-persia-3d-v0-ozn0rqu8dcla1.jpg?width=640&crop=smart&auto=webp&s=3e597ff1bc5609342847536084daf01c51bc8a08'
     ],
 
     related4: [
-      'related3-1.jpg',
-      'related3-2.jpg',
-      'related3-3.jpg'
+      'https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/13600/0000003879.1920x1080.jpg?t=1720535333',
+      'https://i.ytimg.com/vi/7UhV1O5SJaY/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGD8gXihlMA8=&rs=AOn4CLCijVj8s4ftas7Gd3i7skYwG4WNMw',
+      'https://www.speedrun.com/static/theme/mwzg4n8y/background.jpg?v=8b39afc'
   ],
   related5: [
     'related3-1.jpg',
@@ -136,6 +136,54 @@ function imgPrincipale(id){
       
       
       return 'https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/mighoet_sundback_1_prince_of_persia_1989_cover.jpg'
+  }
+  else if(id == '2'){
+    largeImage.style.width = '40%';
+    description.textContent = `Prince of Persia: Shadow and Flame è un videogioco di piattaforme sviluppato da Ubisoft e rilasciato nel 1994, 
+    che rappresenta il seguito del celebre Prince of Persia. In questo gioco, i giocatori riprendono il controllo del Principe, un giovane guerriero che affronta nuove sfide 
+    e avventure. Il gameplay combina elementi di azione, esplorazione e risoluzione di enigmi, con il protagonista che deve navigare attraverso vari livelli, evitando trappole e 
+    combattendo nemici per avanzare nella storia. 
+    Una delle caratteristiche distintive di Shadow and Flame è la sua grafica fluida, realizzata grazie alla tecnologia di rotoscoping, che consente animazioni realistiche dei movimenti
+   dei personaggi. La trama del gioco si sviluppa attorno a temi di vendetta e redenzione, mentre il Principe cerca di superare ostacoli e nemici per raggiungere il suo obiettivo finale.
+    Il gioco ha ricevuto recensioni positive per la sua giocabilità coinvolgente e la sua narrativa avvincente, contribuendo a consolidare il franchise di Prince of Persia
+    e portando a successivi titoli e adattamenti su diverse piattaforme nel corso degli anni. In sintesi, Prince of Persia: Shadow and Flame è un classico dei giochi di piattaforma
+    che riesce a mescolare azione e avventura in un’esperienza memorabile per i fan del genere.`
+
+
+    return 'https://preview.redd.it/temz7plsyc5a1.png?width=640&crop=smart&auto=webp&s=7b9c492f16697ca74b17de514ae3a58c2180ff04';
+  }
+  else if(id == '3'){
+    largeImage.style.width = '30%';
+    description.textContent = `Prince of Persia 3D è un videogioco di azione e avventura sviluppato da Ubisoft e rilasciato nel 1999. Questo titolo rappresenta un importante passo avanti per 
+    la serie, introducendo la grafica 3D e una nuova prospettiva nel gameplay. I giocatori assumono il ruolo del Principe, che deve affrontare il malvagio stregone Kaileena e salvare il suo 
+    regno. Il gameplay di Prince of Persia 3D combina elementi di piattaforma, combattimento e risoluzione di enigmi. I giocatori devono esplorare ambientazioni tridimensionali, 
+    superare trappole mortali e combattere nemici utilizzando una varietà di tecniche e armi. La grafica del gioco è stata apprezzata per il suo design dettagliato e le animazioni fluide, 
+    che hanno dato vita a un mondo affascinante e coinvolgente. La trama del gioco ruota attorno al tema del destino e della redenzione, mentre il Principe cerca di fermare le malefatte del suo avversario e recuperare il controllo del suo regno. Prince of Persia 3D ha ricevuto recensioni miste da parte della critica, con apprezzamenti per il suo innovativo gameplay in 3D, ma anche critiche per alcuni problemi tecnici e di design.
+
+    In sintesi, Prince of Persia 3D rappresenta un capitolo significativo nella storia della serie, combinando azione, avventura e un’ambientazione tridimensionale per offrire un’esperienza di gioco unica per i fan del franchise.`;
+    return 'https://m.media-amazon.com/images/M/MV5BMjM1MzdkNGQtY2UzMi00Y2FjLWFkNmQtYjk2OGViN2ExZmVjXkEyXkFqcGc@._V1_.jpg';
+  }
+
+  else if(id == '4'){
+    largeImage.style.width = '30%';
+    description.textContent =  `Prince of Persia: The Sands of Time è un videogioco d'azione e avventura sviluppato da Ubisoft e rilasciato nel 2003. Questo titolo ha riportato in 
+    auge la serie Prince of Persia, introducendo nuove meccaniche di gioco e una trama coinvolgente. I giocatori vestono i panni del Principe, che si imbatte in un antico 
+    artefatto, la Sabbia del Tempo, che gli consente di controllare il tempo stesso.
+    Il gameplay di The Sands of Time combina elementi di platforming, combattimento e risoluzione di enigmi. I giocatori possono rallentare, fermare o riavvolgere il tempo per superare ostacoli,
+   evitare trappole mortali e combattere nemici. Questa meccanica innovativa ha aggiunto una nuova dimensione all'esperienza di gioco, permettendo ai giocatori di affrontare le sfide 
+   in modi creativi.
+  La trama segue il Principe mentre cerca di salvare il suo regno da un disastro dopo che la Sabbia del Tempo viene accidentalmente liberata. Insieme alla principessa Farah, 
+  il Principe deve affrontare nemici e risolvere enigmi per rimediare al caos causato dall'artefatto. La narrazione è arricchita da un forte sviluppo dei personaggi e da dialoghi accattivanti.
+  Prince of Persia: The Sands of Time è stato acclamato dalla critica per la sua innovativa meccanica di controllo del tempo, la grafica di alta qualità e la giocabilità avvincente. 
+  Ha ricevuto numerosi premi e ha contribuito a rinvigorire la serie, portando a successivi titoli e adattamenti cinematografici. In sintesi, The Sands of Time è un capitolo fondamentale 
+  nella storia di Prince of Persia, combinando azione, avventura e una trama memorabile per offrire un'esperienza di gioco eccezionale.`;
+
+  return 'https://m.media-amazon.com/images/M/MV5BODljYThkMjEtZTY2Zi00ODg5LWE4M2EtZWNkYjVhMzQ5ZDIxXkEyXkFqcGc@._V1_.jpg';
+  }
+
+  else if(id == '5'){
+    largeImage.style.width = '50%';
+    description.textContent = ``;
   }
   else{
       description.textContent = `Descrizione N`;
